bd99929b64905662f95c6780d61dcdd2fd5731ae
Abdurashidov-A <abduqahhor.abdurashidov99@gmail.com>
2025-11-29 17:51:15 +0500

    udemy: version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const messages = [
 
 const App = () => {
   const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
     if (step > 1) setStep((prev) => prev - 1);
   }
@@ -23,40 +23,42 @@ const App = () => {
   }
   return (
     <>
-      <button className="close" onClick={hanleSwitch}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
-      <div className={`steps ${isOpen ? "hide" : ""}`}>
-        <div className="numbers">
-          <div className={step >= 1 && "active"}>1</div>
-          <div className={step >= 2 && "active"}>2</div>
-          <div className={step >= 3 && "active"}>3</div>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 && "active"}>1</div>
+            <div className={step >= 2 && "active"}>2</div>
+            <div className={step >= 3 && "active"}>3</div>
+          </div>
+          <p className="message">
+            {step >= 1 && step <= 3 ? (
+              <span>
+                Step {step}: {messages[step - 1]}
+              </span>
+            ) : (
+              <span>Error</span>
+            )}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+              // onMouseEnter={() => alert("sseekjfjekfeskf")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        <p className="message">
-          {step >= 1 && step <= 3 ? (
-            <span>
-              Step {step}: {messages[step - 1]}
-            </span>
-          ) : (
-            <span>Error</span>
-          )}
-        </p>
-        <div className="buttons">
-          <button
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            onClick={handlePrevious}
-            // onMouseEnter={() => alert("sseekjfjekfeskf")}
-          >
-            Previous
-          </button>
-          <button
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            onClick={handleNext}
-          >
-            Next
-          </button>
-        </div>
-      </div>
+      )}
     </>
   );
 };
