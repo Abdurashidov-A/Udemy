eae4e06e1da266adda7fbb2dc5f326034637936f
Abdurashidov-A <abduqahhor.abdurashidov99@gmail.com>
2025-11-29 15:49:37 +0500

    switch on/off : our own version

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+  function handlePrevious() {
+    if (step > 1) setStep((prev) => prev - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((prev) => prev + 1);
+  }
+
+  function hanleSwitch() {
+    setIsOpen((onoff) => {
+      setIsOpen(!onoff);
+    });
+  }
+  return (
+    <>
+      <button className="close" onClick={hanleSwitch}>
+        &times;
+      </button>
+      <div className={`steps ${isOpen ? "hide" : ""}`}>
+        <div className="numbers">
+          <div className={step >= 1 && "active"}>1</div>
+          <div className={step >= 2 && "active"}>2</div>
+          <div className={step >= 3 && "active"}>3</div>
+        </div>
+        <p className="message">
+          {step >= 1 && step <= 3 ? (
+            <span>
+              Step {step}: {messages[step - 1]}
+            </span>
+          ) : (
+            <span>Error</span>
+          )}
+        </p>
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handlePrevious}
+            // onMouseEnter={() => alert("sseekjfjekfeskf")}
+          >
+            Previous
+          </button>
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
