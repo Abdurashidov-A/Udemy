0f5296ea46c33c86a9a79d796527c37e1cdd3d3e
Abdurashidov-A <abduqahhor.abdurashidov99@gmail.com>
2025-12-11 14:01:26 +0500

    Accordion

--- src/components/Logo.js
+++
@@ -1,3 +0,0 @@
-export const Logo = () => {
-  return <h1>🌴 Far Away 👜</h1>;
-};
--- src/components/test.js
+++ src/components/test.js
@@ -1,59 +1,53 @@
 import { useState } from "react";
+import "./styles.css";
 
 const faqs = [
   {
-    id: 1,
     title: "Where are these chairs assembled?",
     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
-    id: 2,
     title: "How long do I have to return my chair?",
     text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
-    id: 3,
     title: "Do you ship to countries outside the EU?",
     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
   },
 ];
 
-const App = () => {
+export default function App() {
   return (
     <div>
-      <Accardion data={faqs} />
-    </div>
-  );
-};
-
-const Accardion = ({ data }) => {
-  return (
-    <div className="accordion">
-      {data.map((item, idx) => (
-        <AccordionItem item={item} num={idx} />
-      ))}
-    </div>
-  );
-};
-
-function AccordionItem({ item, num }) {
-  const [click, setClick] = useState(false);
-
-  function handleToggle() {
-    setClick((click) => !click);
-  }
-  return (
-    <div
-      key={item.id}
-      className={`item ${click && "open"}`}
-      onClick={handleToggle}
-    >
-      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="title">{item.title}</p>
-      <p className="icon">{click ? "-" : "+"}</p>
-      {click && <div className="content-box">{item.text}</div>}
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
