6f946dde492f19ebebd66a57cfa89f85a9e08377
Abdurashidov-A <abduqahhor.abdurashidov99@gmail.com>
2026-02-03 22:10:52 +0500

    UsePopCorn

--- src/App.js
+++ src/App.js
@@ -1,104 +1,173 @@
 import { useState } from "react";
 
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
 export default function App() {
-  const [items, setItem] = useState([
-    {
-      name: "Banana",
-      quantity: 5,
-      isSelected: true,
-    },
-    {
-      name: "Aplle",
-      quantity: 3,
-      isSelected: true,
-    },
-    {
-      name: "Mango",
-      quantity: 2,
-      isSelected: false,
-    },
-  ]);
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen2, setIsOpen2] = useState(true);
 
-  const [inputValue, setInputValue] = useState("");
-  const [totalValue, setTotalValue] = useState(10);
-
-  function handleAddItem() {
-    const newItem = {
-      name: inputValue,
-      isSelected: false,
-      quantity: 1,
-    };
-
-    setItem([...items, newItem]);
-    setInputValue("");
-  }
-  function handleAdd(index) {
-    const newItem = [...items];
-
-    newItem[index].quantity++;
-
-    setItem(newItem);
-    calculateTotal();
-  }
-  function handleMinus(index) {
-    const newItem = [...items];
-
-    newItem[index].quantity--;
-
-    setItem(newItem);
-    calculateTotal();
-  }
-
-  function handleChangeClass(index) {
-    const newItem = [...items];
-
-    newItem[index].isSelected = !newItem[index].isSelected;
-
-    setItem(newItem);
-  }
-
-  function calculateTotal(item) {
-    const total = items.reduce((total, item) => {
-      return total + item.quantity;
-    }, 0);
-
-    setTotalValue(total);
-  }
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
 
   return (
-    <div className="background">
-      <div className="">
-        <input
-          className="input"
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder=" Add Item..."
-        />
-        <button onClick={handleAddItem}>+</button>
-      </div>
+    <>
+      <Nav query={query} setQuery={setQuery} movies={movies} />
+      <main className="main">
+        <div className="box">
+          <button
+            className="btn-toggle"
+            onClick={() => setIsOpen1((open) => !open)}
+          >
+            {isOpen1 ? "–" : "+"}
+          </button>
+          {isOpen1 && (
+            <ul className="list">
+              {movies?.map((movie) => (
+                <li key={movie.imdbID}>
+                  <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                  <h3>{movie.Title}</h3>
+                  <div>
+                    <p>
+                      <span>🗓</span>
+                      <span>{movie.Year}</span>
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
 
-      <div className="Items">
-        {items.map((item, index) => (
-          <div className="Item">
-            <div
-              className={item.isSelected ? "checkedItem" : "classAddItem"}
-              key={item.index}
-              onClick={() => handleChangeClass(index)}
-            >
-              <input type="checkbox" />
-              <div>{item.name}</div>
-            </div>
+        <div className="box">
+          <button
+            className="btn-toggle"
+            onClick={() => setIsOpen2((open) => !open)}
+          >
+            {isOpen2 ? "–" : "+"}
+          </button>
+          {isOpen2 && (
+            <>
+              <div className="summary">
+                <h2>Movies you watched</h2>
+                <div>
+                  <p>
+                    <span>#️⃣</span>
+                    <span>{watched.length} movies</span>
+                  </p>
+                  <p>
+                    <span>⭐️</span>
+                    <span>{avgImdbRating}</span>
+                  </p>
+                  <p>
+                    <span>🌟</span>
+                    <span>{avgUserRating}</span>
+                  </p>
+                  <p>
+                    <span>⏳</span>
+                    <span>{avgRuntime} min</span>
+                  </p>
+                </div>
+              </div>
 
-            <div className="classAddItem">
-              <button onClick={() => handleMinus(index)}>-</button>
-              {item.quantity}
-              <button onClick={() => handleAdd(index)}>+</button>
-            </div>
-          </div>
-        ))}
-        {totalValue}
+              <ul className="list">
+                {watched.map((movie) => (
+                  <li key={movie.imdbID}>
+                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                    <h3>{movie.Title}</h3>
+                    <div>
+                      <p>
+                        <span>⭐️</span>
+                        <span>{movie.imdbRating}</span>
+                      </p>
+                      <p>
+                        <span>🌟</span>
+                        <span>{movie.userRating}</span>
+                      </p>
+                      <p>
+                        <span>⏳</span>
+                        <span>{movie.runtime} min</span>
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
+
+function Nav(query, setQuery, movies) {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
       </div>
-    </div>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p className="num-results">
+        Found <strong>{movies?.length}</strong> results
+      </p>
+    </nav>
   );
 }
